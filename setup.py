8b788cf5376243fb5e07be61c69c1453ac072759
gissehel <public-devgit-cochir@gissehel.org>
2013-04-14 13:56:13 +0200

    Version 0.1.1 : Trying to make a decent pypi release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='supertools',
-      version='0.1.0',
+      version='0.1.1',
       description='A simple python module to provide .__super ability to python 2 classes.',
       author='Arthibus Gisséhel',
       author_email='public-dev-supertools@gissehel.org',
