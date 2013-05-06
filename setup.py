566bb6f8869082b247d96cb1fd7ccf948e3ba30f
gissehel <public-devgit-cochir@gissehel.org>
2013-05-06 12:02:02 +0200

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='supertools',
-      version='1.0.0',
+      version='1.0.1',
       description='A simple python module to provide .__super ability to python 2 classes.',
       author='Arthibus Gisséhel',
       author_email='public-dev-supertools@gissehel.org',
